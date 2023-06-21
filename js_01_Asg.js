cc4250a91f960d901c6ac0cd854384a0ba31a62c
Sovan Pattanaik <68283456+Sovan2602@users.noreply.github.com>
2023-06-21 17:44:27 +0530

    Add files via upload

---
+++ js_01_Asg.js
@@ -0,0 +1,15 @@
+// Function to calculate the area of a rectangle
+function calculateArea(length, width) {
+    // Calculate the area by multiplying length and width
+    var area = length * width;
+  
+    // Return the calculated area
+    return area;
+  }
+  
+  // Test case
+  var length = 5;
+  var width = 3;
+  var result = calculateArea(length, width);
+  console.log("The area of the rectangle is: " + result);
+  
